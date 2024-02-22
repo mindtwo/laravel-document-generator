b4effeef1bac2098ab768a2a32306f3e5bd932fd
Andreas Schneider <schneider@mindtwo.de>
2024-02-22 10:31:06 +0100

    remove class

--- src/Http/Resources/EditLayoutResource.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace mindtwo\DocumentGenerator\Http\Resources;
-
-use Illuminate\Http\JsonResponse;
-
-class EditLayoutResource extends JsonResponse
-{
-}
--- src/Settings/DocumentSettings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace mindtwo\DocumentGenerator\Settings;
-
-class DocumentSettings
-{
-}
